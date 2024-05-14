c1100bd79c9c2c01c3bf493555340fbda8104160
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-14 12:10:16 +0100

    update

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
+
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import EventItem from './EventItem.jsx';
@@ -7,9 +8,9 @@ import { fetchEvents } from '../../util/http.js';
 export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events', { max: 3 }],
-    queryFn: (signal, queryKey) => fetchEvents({ signal, ...queryKey[1] }),
+    queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] }),
     staleTime: 5000,
-    //gcTime: 1000
+    // gcTime: 1000
   });
 
   let content;
